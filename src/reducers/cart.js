e354042feef8a6438868c45d90835a525b14a9ca
Kevin-AC <kevin.argumedo@gmail.com>
2023-07-02 17:35:23 -0500

    refactor: localStorage

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,10 +1,15 @@
 // Reducer
-export const cartInitialState = []
+export const cartInitialState = JSON.parse(window.localStorage.getItem('cart')) || []
 
 export const CART_ACTION_TYPES = {
   ADD_TO_CART: 'ADD_TO_CART',
   REMOVE_FROM_CART: 'REMOVE_FROM_CART',
   CLEAR_CART: 'CLEAR_CART'
+}
+
+// actualizar local storage
+export const updateLocalStorage = state => {
+  window.localStorage.setItem('cart', JSON.stringify(state))
 }
 
 export const cartReducer = (state, action) => {
@@ -16,22 +21,28 @@ export const cartReducer = (state, action) => {
       if (productInCart >= 0) {
         const newState = structuredClone(state)
         newState[productInCart].quantity += 1
+        updateLocalStorage(newState)
         return newState
       }
 
-      return [
+      const newState = [
         ...state,
         {
           ...actionPayload, // producto
           quantity: 1
         }
       ]
+      updateLocalStorage(newState)
+      return newState
     }
     case CART_ACTION_TYPES.REMOVE_FROM_CART: {
       const { id } = actionPayload
-      return state.filter(item => item.id !== id)
+      const newState = state.filter(item => item.id !== id)
+      updateLocalStorage(newState)
+      return newState
     }
     case CART_ACTION_TYPES.CLEAR_CART: {
+      updateLocalStorage(cartInitialState)
       return cartInitialState
     }
   }
